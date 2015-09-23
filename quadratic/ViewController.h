6abbed4329d342a5670d466ecad2161fc77f8fd8
Roman Gribkov <Roman.Gribkov@gmail.com>
2015-09-23 21:21:54 +0300

    Initial Commit

---
+++ quadratic/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  quadratic
+//
+//  Created by Roman Gribkov on 23.09.15.
+//  Copyright © 2015 Roman Gribkov. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
