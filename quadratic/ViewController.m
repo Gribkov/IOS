ff7f9c919d85c2ee5a102d288d3530bc81edf16e
Roman Gribkov <Roman.Gribkov@gmail.com>
2015-10-12 22:33:33 +0300

    Add comments

--- quadratic/ViewController.m
+++ quadratic/ViewController.m
@@ -34,6 +34,7 @@
 }
 
 - (IBAction)calButton:(id)sender {
+    //Close keyboard
     if ([self.ax isFirstResponder]) {
         [self.ax resignFirstResponder];
     }
@@ -43,9 +44,9 @@
     if ([self.c isFirstResponder]) {
         [self.c resignFirstResponder];
     }
-    double abc;
+    double abc; // Variable for count D, x1 and x2
     abc = ([self.bx.text doubleValue]*[self.bx.text doubleValue])- 4*[self.ax.text doubleValue]*[self.c.text doubleValue];
-    self.D.text = [NSString stringWithFormat:@"%f",abc];
+    self.D.text = [NSString stringWithFormat:@"%f",abc]; // Cound D
     if ([self.D.text doubleValue] == 0) {
         abc = -([self.bx.text doubleValue]/(2*[self.ax.text doubleValue]));
         self.X1.text = [NSString stringWithFormat:@"%f",abc];
@@ -56,9 +57,9 @@
         [self.X2 setText:@"0"];
     }
     if ([self.D.text doubleValue] > 0) {
-        abc = (-[self.bx.text doubleValue]+sqrt(([self.bx.text doubleValue]*[self.bx.text doubleValue])- 4*[self.ax.text doubleValue]*[self.c.text doubleValue]))/(2*[self.ax.text doubleValue]);
+        abc = (-[self.bx.text doubleValue]+sqrt(([self.bx.text doubleValue]*[self.bx.text doubleValue])- 4*[self.ax.text doubleValue]*[self.c.text doubleValue]))/(2*[self.ax.text doubleValue]); //Count X1
         self.X1.text = [NSString stringWithFormat:@"%f", abc];
-        abc = (-[self.bx.text doubleValue]-sqrt(([self.bx.text doubleValue]*[self.bx.text doubleValue])- 4*[self.ax.text doubleValue]*[self.c.text doubleValue]))/(2*[self.ax.text doubleValue]);
+        abc = (-[self.bx.text doubleValue]-sqrt(([self.bx.text doubleValue]*[self.bx.text doubleValue])- 4*[self.ax.text doubleValue]*[self.c.text doubleValue]))/(2*[self.ax.text doubleValue]); //Count X2
         self.X2.text = [NSString stringWithFormat:@"%f",abc];
         
     }
@@ -66,12 +67,14 @@
 
 }
 - (IBAction)clearButton:(id)sender {
+    //Clean input form
     [self.ax setText:@""];
     [self.bx setText:@""];
     [self.c setText:@""];
     [self.D setText:@"D"];
     [self.X1 setText:@"X1"];
     [self.X2 setText:@"X2"];
+    // Close keyboard
     if ([self.ax isFirstResponder]) {
         [self.ax resignFirstResponder];
     }
