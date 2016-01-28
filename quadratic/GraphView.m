c4117dc2a48e5271c24c3e3d9d2830d4c77c9e4a
Roman Gribkov <Roman.Gribkov@gmail.com>
2016-01-28 23:03:10 +0300

    херота

--- quadratic/GraphView.m
+++ quadratic/GraphView.m
@@ -14,6 +14,9 @@
 
 -(void)drawInContext:(CGContextRef) context
 {
+    
+    
+
     CGContextSetRGBStrokeColor(context, 0, 0, 0, 1);
     CGPoint osiPoints[12] = {CGPointMake(10, 120), CGPointMake(230, 120),
         CGPointMake(120, 10), CGPointMake(120, 230),
@@ -42,42 +45,12 @@
         xyPoints[i] = CGPointMake(120, 120);
     }
     
-    CGContextScaleCTM(context,1,-1); //переворачиваю ось игрек что бы текст выводился нормально
-    
-    CGContextSetTextMatrix(context, CGAffineTransformIdentity);
-    CGMutablePathRef path = CGPathCreateMutable();
-    CGRect bounds = CGRectMake(10.0, 10.0, 200.0, 200.0);
-    CGPathAddRect(path, NULL, bounds );
-    CFStringRef textString = CFSTR("Hello, World! I know nothing in the world that has as much power as a word. Sometimes I write one, and I look at it, until it begins to shine.");
-    CFMutableAttributedStringRef attrString = CFAttributedStringCreateMutable(kCFAllocatorDefault, 0);
-    CFAttributedStringReplaceString (attrString, CFRangeMake(0, 0), textString);
-    CGColorSpaceRef rgbColorSpace = CGColorSpaceCreateDeviceRGB();
-    CGFloat components[] = { 1.0, 0.0, 0.0, 0.8 };
-    CGColorRef red = CGColorCreate(rgbColorSpace, components);
-    CGColorSpaceRelease(rgbColorSpace);
-    CFAttributedStringSetAttribute(attrString, CFRangeMake(0, 12), kCTForegroundColorAttributeName, red);
-    CTFramesetterRef framesetter = CTFramesetterCreateWithAttributedString(attrString);
-    CFRelease(attrString);
-    // Create a frame.
-    CTFrameRef frame = CTFramesetterCreateFrame(framesetter, CFRangeMake(0, 0), path, NULL);
-    
-    // Draw the specified frame in the given context.
-    CTFrameDraw(frame, context);
-    
-    // Release the objects we used.
-    /*CFRelease(frame);
-    CFRelease(path);
-    CFRelease(framesetter);*/
-    
-    
-    
-    CGContextScaleCTM(context,1,-1); //переворачиваю ось игрек что бы текст выводился нормально
+
     
     if (self.a != 0 || self.b !=0 || self.c != 0) {
         double j = (totalElements/4)*(-1)/k;
         if (self.x1 != 0 || self.x2 != 0) j = xStart;
-        int i = 0;
-        for (i; i < totalElements; i) {
+        for (int i = 0; i < totalElements; i) {
             xyPoints[i] = CGPointMake(j*k+120, k*((self.a*(j*j)+self.b*j+self.c)*(-1))+120);
             i++;
             j = j+1/k;
@@ -95,6 +68,33 @@
     //CGContextScaleCTM(context,-1,-1);
     CGContextSetRGBFillColor(context, 255, 255, 255, 1);
     CGContextFillRect(context, CGRectMake(1, 1, 238, 238));
+    
+    CGContextScaleCTM(context,1,-1); //переворачиваю ось игрек что бы текст выводился нормально
+    ////////////////////////////////
+    
+    CFStringRef string;
+    CTFontRef font;
+    // Initialize the string, font, and context
+    
+    CFStringRef keys[] = { kCTFontAttributeName };
+    CFTypeRef values[] = { font };
+    
+    CFDictionaryRef attributes = CFDictionaryCreate(kCFAllocatorDefault, (const void**)&keys, (const void**)&values, sizeof(keys) / sizeof(keys[0]),
+                                                    &kCFTypeDictionaryKeyCallBacks, &kCFTypeDictionaryValueCallBacks);
+    
+    CFAttributedStringRef attrString = CFAttributedStringCreate(kCFAllocatorDefault, string, attributes);
+    CFRelease(string);
+    CFRelease(attributes);
+    
+    CTLineRef line = CTLineCreateWithAttributedString(attrString);
+    
+    // Set text position and draw the line into the graphics context
+    CGContextSetTextPosition(context, 10.0, 10.0);
+    CTLineDraw(line, context);
+    CFRelease(line);
+    
+    ////////////////////////////////
+    CGContextScaleCTM(context,1,-1); //переворачиваю ось игрек что бы текст выводился нормально
     
     [self drawInContext:context];
 
